d700b01c09c349d57e278fea7b9d55e1a52b9e8f
Magnar Markvart <73603323+MagnarMarkvart@users.noreply.github.com>
2024-03-30 13:52:32 +0200

    Initial
    Preparation for the project.

---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Query> Queries { get; set; } = default!;
+}
